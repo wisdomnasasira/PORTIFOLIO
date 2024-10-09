8f3cdde5d0d3477608ae513e500902d00a717d63
wisdomnasasira <122895015+wisdomnasasira@users.noreply.github.com>
2024-10-09 17:24:52 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.querySelector('form').addEventListener('submit', function(e) {
+    e.preventDefault(); // Prevent the default form submission
+    alert('Thank you for contacting me!'); // Alert on form submission
+});
